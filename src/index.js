a10d78b5dc52eb52847d58b1d5f4d0c76719e22b
“alex” <poryvvotryv@gmail.com>
2024-03-13 14:55:39 +0200

    Add email checking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const inputs = Array.from(document.querySelectorAll('input'));
 const submit = document.querySelector('button[type=submit]');
+const errorMessage = document.querySelector('span');
 
 class Person {
   constructor(firstName, lastName, email, nickName) {
@@ -14,11 +15,19 @@ class Person {
 
 function getData(e) {
   e.preventDefault();
-  
-  const person = new Person(...inputs.map(el => el.value))
 
-  localStorage.setItem(`${person.lastName}`,
-                       `${JSON.stringify(person)}`);
+  const person = new Person(...inputs.map((el) => el.value));
+
+  localStorage.setItem(`${person.lastName}`, `${JSON.stringify(person)}`);
 }
 
+function checkEmail() {
+  const rexEx = /^\w+\.?\w+@[a-z]{3,8}\.[a-z]{2,5}$/gi;
+
+  rexEx.test(inputs[2].value)
+    ? (errorMessage.style.display = 'none')
+    : (errorMessage.style.display = 'inline-block');
+}
+
+inputs[2].addEventListener('keyup', () => setTimeout(checkEmail, 2000));
 submit.addEventListener('click', getData);
